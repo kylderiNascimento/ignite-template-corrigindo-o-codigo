5c60e9ed27da7f88a3795ce3149d4ee701484bf9
kylderiNascimento <kylderi.nascimento@hotmail.com>
2022-02-23 13:10:42 -0300

    resolvido

--- src/index.js
+++ src/index.js
@@ -1,9 +1,6 @@
 const express = require("express");
-
 const { v4: uuid } = require("uuid");
-
 const app = express();
-
 app.use(express.json());
 
 const repositories = [];
@@ -23,14 +20,22 @@ app.post("/repositories", (request, response) => {
     likes: 0
   };
 
+  repositories.push(repository);
+
   return response.json(repository);
 });
 
 app.put("/repositories/:id", (request, response) => {
   const { id } = request.params;
-  const updatedRepository = request.body;
+  const { title, url, techs } = request.body;
 
-  repositoryIndex = repositories.findindex(repository => repository.id === id);
+  const updatedRepository = {
+    title,
+    url,
+    techs
+  }
+
+  repositoryIndex = repositories.findIndex((repository) => repository.id === id);
 
   if (repositoryIndex < 0) {
     return response.status(404).json({ error: "Repository not found" });
@@ -48,7 +53,7 @@ app.delete("/repositories/:id", (request, response) => {
 
   repositoryIndex = repositories.findIndex(repository => repository.id === id);
 
-  if (repositoryIndex > 0) {
+  if (repositoryIndex < 0) {
     return response.status(404).json({ error: "Repository not found" });
   }
 
@@ -68,7 +73,7 @@ app.post("/repositories/:id/like", (request, response) => {
 
   const likes = ++repositories[repositoryIndex].likes;
 
-  return response.json('likes');
+  return response.json({ likes });
 });
 
 module.exports = app;
